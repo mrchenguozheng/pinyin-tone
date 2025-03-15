f0e7f9a148dd48c9d5f9d6e0c078392e3da79221
Chen Guo Zheng <30428638+mrchenguozheng@users.noreply.github.com>
2025-03-16 02:30:52 +0800

    style: add comments

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,6 @@
-export default function Pinyin(param:string):string;
+/**
+ * Converts the given string to Pinyin.
+ * @param param - The string to be converted.
+ * @returns The converted Pinyin string.
+ */
+export default function Pinyin(param: string): string;
