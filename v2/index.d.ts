8d15b91cd610c98aeac7ae431af71602754446c4
Chen Guo Zheng <30428638+mrchenguozheng@users.noreply.github.com>
2025-10-30 20:41:51 +0800

    update index.d.ts

--- v2/index.d.ts
+++ v2/index.d.ts
@@ -1,3 +1,5 @@
 // v2/index.d.ts
 
 export default function toPinyinTones(longString: string): string;
+export function unspacedPinyinSplit(text: string): string[];
+export function convertUnspacedPinyin(unspacedLongString: string): string;
